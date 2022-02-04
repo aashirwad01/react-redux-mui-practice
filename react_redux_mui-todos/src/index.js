d6e68d8d8cc069cf79357e0c2a6744c4f4be8edb
AASHIRWAD KUMAR <aashirwadkumar159@gmail.com>
2022-02-05 01:57:11 +0530

    Bug in app js ans index js rectified

--- react_redux_mui-todos/src/index.js
+++ react_redux_mui-todos/src/index.js
@@ -3,19 +3,15 @@ import ReactDom from 'react-dom'
 
 import { Provider } from 'react-redux'
 import {store} from './redux/store'
-import ConnectedApp from './app'
+import Todos from './app'
 
 const ReduxApp=()=>{
     return <Provider store={store}>
-        <ConnectedApp/>
+        <Todos/>
     </Provider>
 
 }
 
-const App=()=>{
-    return <div>
-        Hello World
-    </div>
-}
+
 
 ReactDom.render(<ReduxApp/>,document.getElementById('root'))
